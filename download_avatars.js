5b08d7bbe1f8eeae7d9f9a2f6a2a8b647b89fb67
Piotr <bartnic1@gmail.com>
2018-02-19 22:26:09 +0000

    Basic operations of download_avatars completed

--- download_avatars.js
+++ download_avatars.js
@@ -17,26 +17,20 @@ function getRepoContributors(repoOwner, repoName, cb) {
   });
 }
 
-/*
 getRepoContributors("jquery", "jquery", function(err, result) {
+  var entry_path;
   console.log("Errors:", err);
-  //console.log("result:", result);
   var deserialized = JSON.parse(result);
   for (var entry of deserialized){
-    console.log("Avatar_url: ", entry.avatar_url);
-    dwonloadImageByURL(entry.avatar_url, )
+    entry_path = "./avatars/" + entry.login
+    downloadImageByURL(entry.avatar_url, entry_path)
   }
-  return
 });
-*/
 
 function downloadImageByURL(url, filePath) {
-  // ...
   request.get(url)
     .on('error', function(err){
     throw err;
     })
     .pipe(fs.createWriteStream(filePath));
 }
-
-downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
