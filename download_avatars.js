5274efff1b9c8bcaf051e3052e3c1920aaf8422d
Piotr <bartnic1@gmail.com>
2018-02-19 23:35:59 +0000

    Refactored code to use a deserializing function

--- download_avatars.js
+++ download_avatars.js
@@ -8,11 +8,12 @@ var repoName = args[1];
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+//This function retrieves a JSON file from a specific github repoOwner and repoName.
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
-      'User-Agent': 'request',
+      'User-Agent': 'remote',
       'Authorization': token.GITHUB_TOKEN
     }
   };
@@ -20,7 +21,7 @@ function getRepoContributors(repoOwner, repoName, cb) {
     cb(err, body);
   });
 }
-
+//This function downloads an image from a given URL to a relative filepath
 function downloadImageByURL(url, filePath) {
   request.get(url)
     .on('error', function(err){
@@ -28,20 +29,27 @@ function downloadImageByURL(url, filePath) {
     })
     .pipe(fs.createWriteStream(filePath));
 }
+
+//This function covnerts data into a deserialized object format, and calls the
+//download function in order to generate new images in a specified directory
+function deserializeData(err, result){
+  var entry_path;
+  if(err !== null){
+    console.log("Errors:", err);
+  }
+  var deserialized = JSON.parse(result);
+  for (var entry of deserialized){
+    entry_path = "./avatars/" + entry.login;
+    downloadImageByURL(entry.avatar_url, entry_path);
+  }
+  console.log("Avatar download complete!");
+}
+
+//This part runs the code only if key variables are defined from the terminal
 if(repoOwner !== undefined && repoName !== undefined){
-  getRepoContributors(repoOwner, repoName, function(err, result) {
-    var entry_path;
-    if(err !== null){
-      console.log("Errors:", err);
-    }
-    var deserialized = JSON.parse(result);
-    for (var entry of deserialized){
-      entry_path = "./avatars/" + entry.login
-      downloadImageByURL(entry.avatar_url, entry_path)
-    }
-    console.log("Avatar download complete!");
-  });
+  getRepoContributors(repoOwner, repoName, deserializeData);
 }
 else{
   console.log('Invalid input: Must enter "Node download_avatars.js <repoOwner> <repoName>"');
 }
+
