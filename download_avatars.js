c86cb03992f344ac002c7d29e1a203ea910d1d9f
Piotr <bartnic1@gmail.com>
2018-02-19 22:47:17 +0000

    Can now pass in repoowner and name via command line, only runs if they exist

--- download_avatars.js
+++ download_avatars.js
@@ -2,6 +2,10 @@ var request = require('request');
 var token = require('./secrets');
 var fs = require('fs');
 
+var args = process.argv.slice(2);
+var repoOwner = args[0];
+var repoName = args[1];
+
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
@@ -17,20 +21,27 @@ function getRepoContributors(repoOwner, repoName, cb) {
   });
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  var entry_path;
-  console.log("Errors:", err);
-  var deserialized = JSON.parse(result);
-  for (var entry of deserialized){
-    entry_path = "./avatars/" + entry.login
-    downloadImageByURL(entry.avatar_url, entry_path)
-  }
-});
-
 function downloadImageByURL(url, filePath) {
   request.get(url)
     .on('error', function(err){
     throw err;
     })
     .pipe(fs.createWriteStream(filePath));
+}
+if(repoOwner !== undefined && repoName !== undefined){
+  getRepoContributors(repoOwner, repoName, function(err, result) {
+    var entry_path;
+    if(err !== null){
+      console.log("Errors:", err);
+    }
+    var deserialized = JSON.parse(result);
+    for (var entry of deserialized){
+      entry_path = "./avatars/" + entry.login
+      downloadImageByURL(entry.avatar_url, entry_path)
+    }
+    console.log("Avatar download complete!");
+  });
+}
+else{
+  console.log('Invalid input: Must enter "Node download_avatars.js <repoOwner> <repoName>"');
 }
