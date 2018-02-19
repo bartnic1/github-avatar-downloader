21de81b1cc7f954123c38c5d4d2e92a5720187f1
Piotr <bartnic1@gmail.com>
2018-02-19 21:33:27 +0000

    Modified download_avatars to include authorization

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,24 @@
 var request = require('request');
+var token = require('secrets');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    // url: "https://github.com/lighthouse-labs/assessment-exam-student",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': token.GITHUB_TOKEN
+    }
+  };
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
 
